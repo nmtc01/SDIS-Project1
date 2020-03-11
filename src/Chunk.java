507e1445c4e991989908f1c9bfc1b1eeafa4664a
Nuno <up201706162@fe.up.pt>
2020-03-11 13:20:56 +0000

    Added Chunk

---
+++ src/Chunk.java
@@ -0,0 +1,8 @@
+import javafx.util.Pair;
+
+public class Chunk {
+    Pair<Integer, Integer> id;
+    Float size;
+    Integer replication_degree;
+
+}
--- src/TestApp.java
+++ src/TestApp.java
@@ -1,4 +1,4 @@
-public class Main {
+public class TestApp {
 
     public static void main(String[] args) {
 	// write your code here
