b36c5746b8ea1b77949c3a3ebffa8fb00c114f6a
Nuno <up201706162@fe.up.pt>
2020-04-07 20:31:05 +0100

    Break

--- src/Peer.java
+++ src/Peer.java
@@ -63,6 +63,12 @@ public class Peer implements PeerInterface{
             DatagramPacket sendPacket = new DatagramPacket(msg, msg.length);
             SendMessagesManager msgHandler = new SendMessagesManager(sendPacket);
             PeerProtocol.getThreadExecutor().execute(msgHandler);
+            try {
+                Thread.sleep(500);
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
 
         //TODO FINISH
